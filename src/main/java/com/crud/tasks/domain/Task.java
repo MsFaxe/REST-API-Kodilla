138d9040107f5ddd13fd71729e9869896595fd50
MsFaxe <anty.wjj@gmail.com>
2020-08-10 19:33:33 +0200

    23.3 - zadanie

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,44 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import static java.util.Optional.ofNullable;
+
+@Service
+public class SimpleEmailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void send(final Mail mail) {
+        LOGGER.info("Starting email preparation...");
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            if (!ofNullable(mail.getToCc()).isPresent()) {
+                LOGGER.error("No additional recipients");
+            }
+            javaMailSender.send(mailMessage);
+            LOGGER.info("Email has been sent.");
+        } catch (MailException e) {
+            LOGGER.error("Faild to process email sending: " + e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+        return mailMessage;
+    }
+}
