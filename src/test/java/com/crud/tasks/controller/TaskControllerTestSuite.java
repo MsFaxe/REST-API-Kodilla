23b9390a3af3c62f2b34b3df7956df671cd2ef48
MsFaxe <anty.wjj@gmail.com>
2020-09-25 19:56:10 +0200

    29.3 zadanie

--- src/test/java/com/crud/tasks/controller/TaskControllerTestSuite.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTestSuite.java
@@ -1,8 +1,12 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +23,9 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,42 +40,99 @@ public class TaskControllerTestSuite {
     private TaskMapper taskMapper;
 
     @Test
-    public void shouldFetchTasks() {
+    public void shouldFetchTasksList() throws Exception {
         //Given
-        List<Task> tasksList = new ArrayList<>();
-        tasksList.add(new Task(1L, "title", "content"));
+        List<TaskDto> tasksList = new ArrayList<>();
+        tasksList.add(new TaskDto(1L, "title", "content"));
 
-        when(dbService.getAllTasks()).thenReturn(tasksList);
-
-//        List<TrelloListDto> trelloLists = new ArrayList<>();
-//        trelloLists.add(new TrelloListDto("1", "Test list", false));
-//
-//        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
-//        trelloBoards.add(new TrelloBoardDto("1", "Test task", trelloLists));
-//
-//        when(trelloFacade.fetchTrelloBoards()).thenReturn(trelloBoards);
+        when(taskMapper.mapToTaskDtoList(anyList())).thenReturn(tasksList);
 
         //When & Then
         mockMvc.perform(get("/v1/task/getTasks").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                //Trello board fields
                 .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id", is("1L")))
-                .andExpect(jsonPath("$[0].title", is("title")));
-                //Trello list fields
-                .andExpect(jsonPath("$[0].lists", hasSize(1)))
-                .andExpect(jsonPath("$[0].lists[0].id", is("1")))
-                .andExpect(jsonPath("$[0].lists[0].name", is("Test list")))
-                .andExpect(jsonPath("$[0].lists[0].closed", is(false)));
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("title")))
+                .andExpect(jsonPath("$[0].content", is("content")));
     }
 
     @Test
-    public void shouldCreateTask() {
+    public void shouldFetchTask() throws Exception {
         //Given
+        TaskDto taskDto = new TaskDto(1L, "title", "content");
+        List<TaskDto> tasksList = new ArrayList<>();
+        tasksList.add(taskDto);
 
-        //When
+        when(taskMapper.mapToTaskDtoList(anyList())).thenReturn(tasksList);
+        when(taskMapper.mapToTaskDto(any())).thenReturn(taskDto);
 
-        //Then
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
 
+        //When & Then
+        mockMvc.perform(get("/v1/taskDto/getTask")
+                .param("taskId", "1L")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("title")))
+                .andExpect(jsonPath("$[0].content", is("content")));
+    }
+
+    @Test
+    public void shouldUpdateTask() throws Exception {
+        //Given
+        Task task = new Task(1L, "title", "content");
+
+        when(taskMapper.mapToTask(any(TaskDto.class))).thenReturn(task);
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(task);
+        //When & Then
+        mockMvc.perform(put("/v1/task/updateTask")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCreateTask() throws Exception {
+        //Given
+        Task task = new Task(1L, "title", "content");
+
+        when(taskMapper.mapToTask(any(TaskDto.class))).thenReturn(task);
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(task);
+        //When & Then
+        mockMvc.perform(post("/v1/task/createTask")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldDeleteTask() throws Exception{
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title", "content");
+        List<TaskDto> tasksList = new ArrayList<>();
+        tasksList.add(taskDto);
+
+        when(taskMapper.mapToTaskDtoList(anyList())).thenReturn(tasksList);
+        when(taskDto.getId()).thenReturn(1L);
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(tasksList);
+        //When & Then
+        mockMvc.perform(delete("/v1/task/deleteTask")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk());
     }
 }
