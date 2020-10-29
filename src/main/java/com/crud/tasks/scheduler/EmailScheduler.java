7b08cb965b063b9123f62e941835cc6542754c26
MsFaxe <anty.wjj@gmail.com>
2020-10-29 11:35:09 +0100

    32.3 - zadanie

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -3,28 +3,28 @@ package com.crud.tasks.scheduler;
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
-import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.service.DailyEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailScheduler {
-    private static final String SUBJECT = "Tasks: Once a day email";
+    private static final String SUBJECT = "Tasks: Daily Report";
 
     @Autowired
-    private SimpleEmailService simpleEmailService;
+    private DailyEmailService dailyEmailService;
     @Autowired
     private TaskRepository taskRepository;
     @Autowired
     private AdminConfig adminConfig;
 
-   // @Scheduled(cron = "0 0 10 * * *")
-    //@Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * *") //once a day
+    //@Scheduled(fixedDelay = 60000) //once a minute
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String END_SENTENCE = size>1? " tasks" : " task";
-        simpleEmailService.send(new Mail(
+        dailyEmailService.sendDailyReport(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
                 "Currently in database you got: " + size + END_SENTENCE));
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
     @Autowired
@@ -16,6 +19,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your task");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
@@ -24,6 +33,22 @@ public class MailCreatorService {
         context.setVariable("goodbye", "Best regards");
         context.setVariable("company_name", adminConfig.getCompanyName());
         context.setVariable("company_mail", adminConfig.getCompanyMail());
+        context.setVariable("show_button", false);
+        context.setVariable("if_friend", true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/create-trello-card-mail", context);
+    }
+
+    public String dailyReportEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Best regards");
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("company_mail", adminConfig.getCompanyMail());
+        context.setVariable("show_button", true);
+        return templateEngine.process("mail/daily-report-mail", context);
     }
 }
